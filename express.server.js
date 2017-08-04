162fb4ab8160da1e9730de9443c2d8d98d959f51
SNOODBOOK\MiniSnood <duncaniaria@gmail.com>
2017-08-03 21:30:05 -0400

    initial commit

---
+++ express.server.js
@@ -0,0 +1,29 @@
+const express = require( "express" );
+const fs = require( "fs" );
+const server = express();
+const PORT = 4001;
+
+server.get( "/", onHomePage );
+
+function onHomePage( tRequest, tResponse )
+{
+    onReadFile( 'index.html', tResponse );
+}
+
+function onReadFile( tFile, tResponse )
+{
+    fs.readFile( tFile, onReadComplete )
+    
+    function onReadComplete( tError, tData )
+    {
+        tResponse.writeHead( 200, { 'Content-Type': 'text/html' } );
+        tResponse.end( tData );
+    }
+}
+
+server.listen( PORT, onServerInit );
+
+function onServerInit()
+{
+    console.log( "what it is?" );
+}
